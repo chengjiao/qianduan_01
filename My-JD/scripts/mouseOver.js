b7522e29abd02b61b3d2d78a520b336465b5ba6c
chengjiao <chengjiaosky@gmail.com>
2016-03-17 22:28:38 +0800

    no message

--- My-JD/scripts/mouseOver.js
+++ My-JD/scripts/mouseOver.js
@@ -3,6 +3,7 @@ function mouseOver(){
 	dt.onmouseover = function(){
 		var ui = document.getElementById("item-ff");
 		ui.style.display = "block";
+		ui.style.z.index = 100;
 	}
 	dt.onmouseout = function(){
 		var ui = document.getElementById("item-ff");
